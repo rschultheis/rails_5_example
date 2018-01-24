7e842840d15433a51ddfb3b18cb9c8c55ff241b8
Robert Schultheis <rschultheis@github.com>
2018-01-24 18:25:49 -0500

    added dynamic data to controller

--- app/controllers/example_controller.rb
+++ app/controllers/example_controller.rb
@@ -1,4 +1,5 @@
 class ExampleController < ApplicationController
   def test
+    @time = Time.now
   end
 end
--- test/controllers/example_controller_test.rb
+++ test/controllers/example_controller_test.rb
@@ -6,4 +6,10 @@ class ExampleControllerTest < ActionDispatch::IntegrationTest
     assert_response :success
   end
 
+  test "should have the date" do
+    now_time = Time.now.to_s
+    get example_test_url
+    assert_includes @response.body, now_time
+  end
+
 end
